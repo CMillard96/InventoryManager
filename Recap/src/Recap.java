333474302fc1735b8bcd1596ced0afbc7fa54a2b
Chad Millard <chadpmillard@hotmail.com>
2020-05-28 11:57:03 +0100

    Convert queue to stack

---
+++ Recap/src/Recap.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.*;
+public class Recap {
+	
+	
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> names = new LinkedList<String>();
+		names.push("Chad");
+		names.push("Nicky");
+		names.push("Paul");
+		
+		System.out.println(names.pop());
+		System.out.println(names.remove());
+		System.out.println(names.removeFirst());
+		
+		
+		
+		
+	}
+
+}
