3ff3cfc8faaa2642db7500270cf0c2cd33a4f23f
Chad Millard <chadpmillard@hotmail.com>
2020-05-29 11:59:26 +0100

    Working with 2d arrays

--- Recap/src/Recap.java
+++ Recap/src/Recap.java
@@ -1,36 +1,22 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Scanner;
 
-import javax.swing.*;
-public class Recap {
-	
-	
+
+public class Recap {	
 
 	public static void main(String[] args) {
 		
-		LinkedList<String> names = new LinkedList<String>();
-		names.push("Chad");
-		names.push("Nicky");
-		names.push("Paul");
+		int[][] data = {
+				{4, 6, 3, 10},
+				{4, 2, 40, 1, 5, 3, 2, 30},
+				{5, 34, 1, 43}
+		};
 		
-		ListIterator<String> it = names.listIterator();
-		it.next();
-		it.next();
-		it.add("Susan");
-			
-		for(String s: names) {
-			System.out.println(s);
+		
+		for(int i = 0; i < data.length; i++) {
+			for(int j = 0; j < data[i].length; j++) {
+				System.out.print(data[i][j] + " ");				
+			}
+			System.out.println();
 		}
-		
-		
-		
-		
 	}
 
 }
